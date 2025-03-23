f68dd32e15aca8375972a392722ec9c7af76be0f
Samuel Onoja <samuelonoja0@gmail.com>
2025-03-23 05:41:05 -0700

    implement contact schema

---
+++ models/Contact.schema.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose')
+
+const contactSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please enter a valid email address']
+    },
+    message: {
+        type: String,
+        required: true 
+    }
+})
+
+const Contact = model('Contact', contactSchema)
+module.exports = Contact; 
