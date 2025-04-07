c5ab30a583419a276ebd899f0b6bc8409a430ae7
Samuel Onoja <samuelonoja0@gmail.com>
2025-04-07 05:52:12 -0700

    update contact route with nodemailer

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ require("dotenv").config();
 
 // ℹ️ Connects to the database
 require("./db");
+const cors = require("cors");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
 
 const app = express();
+
+app.use(cors());
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 const mongoose = require('mongoose')
 
 const Contact = require('../models/Contact.model')
+
+// Nodemailer configuration
+const transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+  });
 
 //create a contact form
 router.post('/contact', (req, res, next) => {
@@ -15,6 +25,22 @@ router.post('/contact', (req, res, next) => {
     })
     .then( (contactDB) => {
         res.status(201).json(contactDB)
+        const mailOptions = {
+            from: process.env.EMAIL_USER, 
+            to: 'samuelonoja2@gmail.com',
+            subject: 'New Contact Form Portfolio', 
+            text: `You have a new message from ${fullname} (${email}):\n\n${message}`,
+          };
+          // Send email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log('Error sending email:', error);
+          return res.status(500).json({ message: 'Error sending email' });
+        }
+        console.log('Email sent:', info.response);
+        res.status(201).json(contactDB);
+      });
+
     }).catch( (e) => {
         console.log('error trying to contact', e)
         res.status(500).json({message: 'error trying to contact'})
